ca22655d3b9b5545c4e51ce8de2f467f9862c424
Flartiny <1335348298@qq.com>
2026-07-03 19:48:49 +0800

    chore: dynamic read version

---
+++ app.config.js
@@ -0,0 +1,47 @@
+const { version } = require("./package.json");
+
+module.exports = {
+  expo: {
+    name: "Bangumini",
+    slug: "bangumini-for-android",
+    scheme: "bangumini",
+    version,
+    icon: "./assets/icon.png",
+    orientation: "portrait",
+    userInterfaceStyle: "dark",
+    newArchEnabled: false,
+    assetBundlePatterns: ["**/*"],
+    plugins: [
+      "./plugins/withAndroidConfig",
+      "expo-router",
+      [
+        "expo-font",
+        {
+          fonts: [
+            "./node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/Ionicons.ttf",
+            "./node_modules/@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialCommunityIcons.ttf",
+          ],
+        },
+      ],
+      "expo-secure-store",
+      "expo-sqlite",
+      [
+        "expo-splash-screen",
+        {
+          backgroundColor: "#101216",
+          image: "./assets/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+        },
+      ],
+    ],
+    android: {
+      package: "dev.raycast.bangumini",
+      adaptiveIcon: {
+        backgroundColor: "#101216",
+        foregroundImage: "./assets/adaptive-icon.png",
+      },
+      permissions: ["android.permission.INTERNET"],
+    },
+  },
+};
